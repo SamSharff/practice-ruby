bfaa4413427f4c441e5851e615e942e5f6882839
Sam Sharff <samanthaleec@gmail.com>
2022-10-13 12:49:17 -0400

    adds review annual raise methods

--- employee.rb
+++ employee.rb
@@ -9,10 +9,26 @@ class Employee
   def print_info
     puts "#{@first_name} #{@last_name} makes #{@salary} dollars per year. She is #{@active}."
   end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+    puts "#{@first_name} #{@last_name}'s raise this year will total #{@salary}"
+  end
+
+  def review
+    if @active == true
+      puts "schedule for a review in six months"
+    else
+      puts "this person is inactive and doesn't require a review"
+    end
+  end
 end
 
 employee1 = Employee.new("majora", "campbell", 80000, true)
-employee2 = Employee.new("denilo", "ramirez", 70000, true)
+employee2 = Employee.new("denilo", "ramirez", 70000, false)
 
-employee1.print_info
-employee2.print_info
+# employee1.print_info
+# employee2.print_info
+
+# employee1.give_annual_raise
+employee2.review
