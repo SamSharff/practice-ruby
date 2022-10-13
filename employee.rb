781b280b9ff845cf386b4f2e766ff8bd4d47dfc4
Sam Sharff <samanthaleec@gmail.com>
2022-10-13 14:19:07 -0400

    adds attr_reader & attr_writer shortcuts to Employee class

--- employee.rb
+++ employee.rb
@@ -1,4 +1,7 @@
 class Employee
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
@@ -7,21 +10,37 @@ class Employee
   end
 
   def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} dollars per year. She is #{@active}."
+    puts "#{first_name} #{last_name} makes #{@salary} dollars per year. She is #{active}."
   end
 
   def give_annual_raise
-    @salary = @salary * 1.05
-    puts "#{@first_name} #{@last_name}'s raise this year will total #{@salary}"
+    @salary = 1.05 * @salary
+    # puts "#{first_name} #{last_name}'s raise this year will total #{@salary}."
   end
 
   def review
-    if @active == true
+    if active == true
       puts "schedule for a review in six months"
     else
       puts "this person is inactive and doesn't require a review"
     end
   end
+
+  # def first_name
+  #   @first_name
+  # end
+
+  # def last_name
+  #   @last_name
+  # end
+
+  # def active
+  #   @active
+  # end
+
+  # def active=(input_active)
+  #   @active = input_active
+  # end
 end
 
 employee1 = Employee.new("majora", "campbell", 80000, true)
@@ -29,6 +48,17 @@ employee2 = Employee.new("denilo", "ramirez", 70000, false)
 
 # employee1.print_info
 # employee2.print_info
+# employee2.give_annual_raise
 
 # employee1.give_annual_raise
-employee2.review
+# employee2.review
+
+# pp employee1.give_annual_raise
+# puts employee2.first_name
+
+employee1.active = false
+p employee1.active
+p employee1.give_annual_raise
+employee1.active = true
+employee1.print_info
+# puts employee2.last_name
