c32190d2872481b2583bca89a678977f389e4aa6
Sam Sharff <samanthaleec@gmail.com>
2022-10-19 13:13:01 -0400

    creates manager class

--- manager.rb
+++ manager.rb
@@ -1,4 +1,4 @@
-class Employee
+class Manager
   attr_reader :first_name, :last_name, :active
   attr_writer :active
 
@@ -16,39 +16,43 @@ class Employee
   def give_annual_raise
     @salary = 1.05 * @salary
   end
-end
-
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
-
-class Manager < Employee
-  def initialize(input_options)
-    super
-    @employees = input_options[:employees]
-  end
 
   def send_report
-    puts "Sending email..."
-    # use email sending library...
-    puts "Email sent!"
+    puts "sending email..."
+    # use email sending library
+    puts "Email sent"
   end
 end
 
-def give_all_raises
-  puts "gonna give all raises!"
-  @employees[0].give_annual_raise
-  @employees[1].give_annual_raise
-end
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
 
-def fire_all_employees
-
-end
-
-
-
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
-manager.give_all_raises
+
+# class Manager < Employee
+#   def initialize(input_options)
+#     super
+#     @employees = input_options[:employees]
+#   end
+
+#   def send_report
+#     puts "Sending email..."
+#     # use email sending library...
+#     puts "Email sent!"
+#   end
+# end
+
+# def give_all_raises
+#   puts "gonna give all raises!"
+#   @employees[0].give_annual_raise
+#   @employees[1].give_annual_raise
+# end
+
+# def fire_all_employees
+
+# end
+
+# manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
+# manager.print_info
+# manager.send_report
+# manager.give_all_raises
