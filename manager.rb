180a410d79d635644e535308bd9dd01660ab4d50
Sam Sharff <samanthaleec@gmail.com>
2022-10-19 13:21:28 -0400

    corrects manager.rb to allow manager to inherit from employee class

--- manager.rb
+++ manager.rb
@@ -1,4 +1,4 @@
-class Manager
+class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
 
@@ -10,13 +10,23 @@ class Manager
   end
 
   def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+    puts "#{first_name} #{last_name} makes #{@salary} dollars per year. She is #{active}."
   end
 
   def give_annual_raise
     @salary = 1.05 * @salary
   end
 
+  def review
+    if active == true
+      puts "schedule for a review in six months"
+    else
+      puts "this person is inactive and doesn't require a review"
+    end
+  end
+end
+
+class Manager < Employee
   def send_report
     puts "sending email..."
     # use email sending library
@@ -25,31 +35,17 @@ class Manager
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
+manager2 = Manager.new(first_name: "Jen", last_name: "VanHorn", salary: 100000, active: false)
 
-manager.print_info
-manager.send_report
-
-# class Manager < Employee
-#   def initialize(input_options)
-#     super
-#     @employees = input_options[:employees]
-#   end
-
-#   def send_report
-#     puts "Sending email..."
-#     # use email sending library...
-#     puts "Email sent!"
-#   end
-# end
+manager2.print_info
+manager2.send_report
+p manager2.first_name
+p manager2.review
 
 # def give_all_raises
 #   puts "gonna give all raises!"
 #   @employees[0].give_annual_raise
 #   @employees[1].give_annual_raise
-# end
-
-# def fire_all_employees
-
 # end
 
 # manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
